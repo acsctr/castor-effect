0bda06e5667979ce236bec5f7261e195c179910c
Aurélie Constant <acs@audaxis.com>
2024-06-03 15:23:59 +0200

    Intégration genially

--- src/app/castorclic.tsx
+++ src/app/castorclic.tsx
@@ -1,7 +1,13 @@
 export default function Castorclic() {
     return (
         <div className="castorclic">
-            <iframe className="castoriframe" title="castor" frameBorder="0" width="1200" height="auto" src="https://view.genially.com/665989492424b30015efa4e3" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+            <iframe
+                src="https://view.genially.com/665989492424b30015efa4e3"
+                width="100%"
+                height="400"
+                allowFullScreen
+                title="Tout savoir sur le castor"
+            />
         </div>
     );
 }
