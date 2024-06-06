68f82e6a67d1ee5451c0546ef83926ec1ed57d9a
Aurélie Constant <acs@audaxis.com>
2024-06-06 09:07:05 +0200

    Custom colors

--- src/app/barrages.tsx
+++ src/app/barrages.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Barrages() {
     return (
-        <div className="barrages flex justify-center">
+        <div className="barrages flex justify-center bg-green-500/90">
             <Image
                 src="/images/barrage_infographie.png"
                 fill={true}
